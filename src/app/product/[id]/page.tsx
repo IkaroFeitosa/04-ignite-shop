148749ef0f5e05f563f48a688d8f2b40009cbf4c
Ikaro Feitosa <ikaro.feitosa.ext@espm.br>
2025-10-17 15:18:03 -0300

    Produto & Checkout - Estrutura da página de produto

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,3 +1,5 @@
+import styles from "@/styles/pages/product.module.scss";
+
 interface ProductPageProps {
   params: {
     id: string;
@@ -5,5 +7,18 @@ interface ProductPageProps {
 }
 
 export default function ProductPage({ params }: ProductPageProps) {
-  return <div>Product Page: {params.id}</div>;
+  return (
+    <main className={styles.ProductContainer}>
+      <div className={styles.ImageContainer}>{/* Imagem do produto */}</div>
+      <div className={styles.ProductDetails}>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <button>Comprar agora</button>
+      </div>
+    </main>
+  );
 }
